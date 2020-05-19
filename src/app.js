2aeb34d5df4a36de9668a6757c48d08422d4375c
itellyou <owner@itellyou.com>
2020-05-19 23:13:23 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,3 +3,10 @@ export async function getInitialState() {
         me:null,//window.g_initialProps.user.me,
     };
 }
+
+export const ssr = {
+    modifyGetInitialPropsCtx: async (ctx) => {
+        console.log(ctx,12313)
+        ctx.test = "test"
+    }
+}
