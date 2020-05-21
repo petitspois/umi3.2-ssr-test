350e53deaf5b46165296e02cf13a46384ab9622b
itellyou <owner@itellyou.com>
2020-05-21 08:47:03 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,3 @@
-export async function getInitialState() {
-    return {
-        me:null,//window.g_initialProps.user.me,
-    };
-}
-
 export const ssr = {
     modifyGetInitialPropsCtx: async (ctx) => {
         console.log(ctx,12313)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import styles from './index.less';
 
-export default () => {
+const Page = (props:any) => {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>{props.text}</h1>
     </div>
   );
 }
+Page.getInitialProps = async (cxt:any) => {
+    console.log("test value:",cxt.test)
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve({text:"Timeout Hello"})
+        }, 2000);
+    })
+}
+export default Page
